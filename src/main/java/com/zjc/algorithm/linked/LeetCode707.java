c8b1566fa67c0e1c97854bc7fb9c707314925c0b
zoujianchao <data_zjc@163.com>
2021-08-03 11:26:41 +0800

    LeetCode707

---
+++ src/main/java/com/zjc/algorithm/linked/LeetCode707.java
@@ -0,0 +1,73 @@
+package com.zjc.algorithm.linked;
+
+/**
+ * @author : zoujianchao
+ * @version : 1.0
+ * @date : 2021/8/3
+ * @description : LeetCode707 设计链表
+ */
+public class LeetCode707 {
+    int size;
+    ListNode head;
+    /** Initialize your data structure here. */
+    public LeetCode707() {
+        size = 0;
+        head = new ListNode(0);
+    }
+    
+    /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+    public int get(int index) {
+        if (index < 0 || index >= size) {
+            return -1;
+        }
+        ListNode cur = head;
+        for (int i = 0; i <= index; i++) {
+            cur = cur.next;
+        }
+        return cur.val;
+    }
+    
+    /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+    public void addAtHead(int val) {
+        addAtIndex(0, val);
+    }
+    
+    /** Append a node of value val to the last element of the linked list. */
+    public void addAtTail(int val) {
+        addAtIndex(size, val);
+    }
+    
+    /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+    public void addAtIndex(int index, int val) {
+        if (index > size) {
+            return;
+        }
+        if (index < 0) {
+            index = 0;
+        }
+        size++;
+        //找到要插入节点的前驱
+        ListNode pre = head;
+        for (int i = 0; i < index; i++) {
+            pre = pre.next;
+        }
+        ListNode toAdd = new ListNode(val);
+        toAdd.next = pre.next;
+        pre.next = toAdd;
+    }
+    
+    /** Delete the index-th node in the linked list, if the index is valid. */
+    public void deleteAtIndex(int index) {
+        if (index < 0 || index >= size) {
+            return;
+        }
+        size--;
+        ListNode pre = head;
+        for (int i = 0; i < index; i++) {
+            pre = pre.next;
+        }
+        pre.next = pre.next.next;
+    }
+}
+
+
