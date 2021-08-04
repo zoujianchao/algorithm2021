644ac689c98c70e04b727359bf5965344225604a
zoujianchao <data_zjc@163.com>
2021-08-04 10:18:13 +0800

    LeetCode18

--- src/main/java/com/zjc/algorithm/hash/LeetCode18.java
+++ src/main/java/com/zjc/algorithm/hash/LeetCode18.java
@@ -1,35 +1,49 @@
 package com.zjc.algorithm.hash;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author : zoujianchao
  * @version : 1.0
- * @date : 2021/8/3
+ * @date : 2021/8/4
  * @description : 四数之和
  */
 public class LeetCode18 {
-    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int res = 0;
-        int sumA = 0;
-        int sumB = 0;
-        for (int a : nums1) {
-            for (int b : nums2) {
-                sumA = a + b;
-                if (map.containsKey(sumA)) {
-                    map.put(sumA, map.get(sumA) + 1);
-                }else {
-                    map.put(sumA, 1);
-                }
-            }
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums.length < 4) {
+            return res;
         }
-        for (int c : nums3) {
-            for (int d : nums4) {
-                sumB = -(c + d);
-                if (map.containsKey(sumB)) {
-                    res += map.get(sumB);
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = nums.length - 1; j >= 0; j--) {
+                if (j < nums.length - 1 && nums[j] == nums[j + 1]) {
+                    continue;
+                }
+                int g = i + 1;
+                int k = j - 1;
+                while (k > g) {
+                    if (nums[i] + nums[j] + nums[g] + nums[k] == target) {
+                        List<Integer> list = new ArrayList<>();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[g]);
+                        list.add(nums[k]);
+                        res.add(list);
+                        g++;
+                        k--;
+                        while (g < k && nums[g] == nums[g - 1]) g++;
+                        while (g < k && nums[k] == nums[k + 1]) k--;
+                    }else if (nums[i] + nums[j] + nums[g] + nums[k] > target) {
+                        k--;
+                    }else {
+                        g++;
+                    }
                 }
             }
         }
